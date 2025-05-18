54e4acb5addd98e0c6f97363e503652d2b4e6a64
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-05-18 23:15:28 +0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -66,7 +66,7 @@ while len(tmp) != 1 and flag < 256:
 
     # Собираем пары для других M и C
     possible_key = [i for i, val in enumerate(arr) if val]
-    print("Indexes: " + str(possible_key))
+    #print("Indexes: " + str(possible_key))
     arr1 = [[0 for i in range(1024)] for j in range(1024)]
     V = [0 for p in range(1024)]
 
